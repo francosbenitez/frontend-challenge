9abdce14f056be33b364e6fea3ad1af22a351449
francobenitez <franco.benitez@hogarthww.com>
2022-05-04 18:55:41 -0300

    feat: clean up & add first components

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,23 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <PrimarySectionDescription />
+    <PrimarySectionImage />
   </div>
 </template>
 
 <script>
+import PrimarySectionDescription from "./components/PrimarySectionDescription.vue";
+import PrimarySectionImage from "./components/PrimarySectionImage.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    PrimarySectionDescription,
+    PrimarySectionImage,
+  },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ vue-app/src/components/PrimarySectionImage.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>PrimarySectionImage</div>
+</template>
