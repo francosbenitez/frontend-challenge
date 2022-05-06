973fbd5b020b10ec27da63d96d1eddc06b1e787f
francobenitez <franco.benitez@hogarthww.com>
2022-05-06 00:07:51 -0300

    feat: improve SecondarySection component

--- vue-app/src/components/SecondarySection.vue
+++ vue-app/src/components/SecondarySection.vue
@@ -1,7 +1,11 @@
 <template>
-  <section>
-    <h2>Hay un mundo<span>ENORME</span></h2>
-    <div>
+  <section class="section-secondary">
+    <h2 class="section-secondary__title">
+      Hay un mundo<br />
+      <span class="section-secondary__title--special"> ENORME</span>
+    </h2>
+    <div class="section-secondary__images"></div>
+    <div class="section-secondary__networks">
       <div>
         <img src="/assets/images/icon-0.svg" />
         <div>
@@ -19,3 +23,28 @@
     </div>
   </section>
 </template>
+
+<style lang="scss">
+$color__primary: #335a39;
+
+.section-secondary {
+  .section-secondary__title {
+    font-family: "Poppins Black";
+    font-size: 30px;
+    color: $color__primary;
+    line-height: 46px;
+    height: 86px;
+    width: 330px;
+    margin: 0 auto;
+
+    .section-secondary__title--special {
+      letter-spacing: 7.4px;
+    }
+  }
+
+  .section-secondary__networks {
+    margin: 0 auto;
+    width: 330px;
+  }
+}
+</style>
