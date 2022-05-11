51e57b996ce9cffe456fa296cb64d99b38d6c247
francobenitez <franco.benitez@hogarthww.com>
2022-05-11 13:07:50 -0300

    refactor: refactor animations scss in PrimarySection

--- vue-app/src/components/PrimarySection.vue
+++ vue-app/src/components/PrimarySection.vue
@@ -85,68 +85,68 @@ export default {
   background-color: $color__primary;
   color: $color__white;
 
+  &__initial-animation {
+    .section--primary__image-background {
+      -webkit-animation: scale-in-br 0.8s ease-out both;
+      animation: scale-in-br 0.8s ease-out both;
+      -webkit-animation-delay: 0.05s;
+      animation-delay: 0.05s;
+    }
+
+    .section--primary__menu {
+      -webkit-animation: slide-in-right 0.8s ease-out both;
+      animation: slide-in-right 0.8s ease-out both;
+      -webkit-animation-delay: 0.25s;
+      animation-delay: 0.25s;
+    }
+
+    .section--primary__btn--left {
+      -webkit-animation: slide-in-bottom 0.8s ease-out both;
+      animation: slide-in-bottom 0.8s ease-out both;
+      -webkit-animation-delay: 0.3s;
+      animation-delay: 0.3s;
+    }
+
+    .section--primary__btn--right {
+      -webkit-animation: slide-in-bottom 0.8s ease-out both;
+      animation: slide-in-bottom 0.8s ease-out both;
+      -webkit-animation-delay: 0.3s;
+      animation-delay: 0.3s;
+    }
+
+    .section--primary__title {
+      -webkit-animation: slide-in-bottom 0.8s ease-out both;
+      animation: slide-in-bottom 0.8s ease-out both;
+      -webkit-animation-delay: 0.1s;
+      animation-delay: 0.1s;
+    }
+
+    .section--primary__line {
+      -webkit-animation: slide-in-bottom 0.8s ease-out both;
+      animation: slide-in-bottom 0.8s ease-out both;
+      -webkit-animation-delay: 0.15s;
+      animation-delay: 0.15s;
+    }
+
+    .section--primary__description {
+      -webkit-animation: slide-in-bottom 0.8s ease-out both;
+      animation: slide-in-bottom 0.8s ease-out both;
+      -webkit-animation-delay: 0.2s;
+      animation-delay: 0.2s;
+    }
+
+    .section--primary__networks {
+      -webkit-animation: slide-in-bottom 0.8s ease-out both;
+      animation: slide-in-bottom 0.8s ease-out both;
+      -webkit-animation-delay: 0.25s;
+      animation-delay: 0.25s;
+    }
+  }
+
   @media (min-width: $breakpoint--lg) {
     display: flex;
     flex-wrap: wrap;
     position: relative;
-  }
-}
-
-.section--primary__initial-animation {
-  .section--primary__image-background {
-    -webkit-animation: scale-in-br 0.8s ease-out both;
-    animation: scale-in-br 0.8s ease-out both;
-    -webkit-animation-delay: 0.05s;
-    animation-delay: 0.05s;
-  }
-
-  .section--primary__menu {
-    -webkit-animation: slide-in-right 0.8s ease-out both;
-    animation: slide-in-right 0.8s ease-out both;
-    -webkit-animation-delay: 0.25s;
-    animation-delay: 0.25s;
-  }
-
-  .section--primary__btn--left {
-    -webkit-animation: slide-in-bottom 0.8s ease-out both;
-    animation: slide-in-bottom 0.8s ease-out both;
-    -webkit-animation-delay: 0.3s;
-    animation-delay: 0.3s;
-  }
-
-  .section--primary__btn--right {
-    -webkit-animation: slide-in-bottom 0.8s ease-out both;
-    animation: slide-in-bottom 0.8s ease-out both;
-    -webkit-animation-delay: 0.3s;
-    animation-delay: 0.3s;
-  }
-
-  .section--primary__title {
-    -webkit-animation: slide-in-bottom 0.8s ease-out both;
-    animation: slide-in-bottom 0.8s ease-out both;
-    -webkit-animation-delay: 0.1s;
-    animation-delay: 0.1s;
-  }
-
-  .section--primary__line {
-    -webkit-animation: slide-in-bottom 0.8s ease-out both;
-    animation: slide-in-bottom 0.8s ease-out both;
-    -webkit-animation-delay: 0.15s;
-    animation-delay: 0.15s;
-  }
-
-  .section--primary__description {
-    -webkit-animation: slide-in-bottom 0.8s ease-out both;
-    animation: slide-in-bottom 0.8s ease-out both;
-    -webkit-animation-delay: 0.2s;
-    animation-delay: 0.2s;
-  }
-
-  .section--primary__networks {
-    -webkit-animation: slide-in-bottom 0.8s ease-out both;
-    animation: slide-in-bottom 0.8s ease-out both;
-    -webkit-animation-delay: 0.25s;
-    animation-delay: 0.25s;
   }
 }
 </style>
